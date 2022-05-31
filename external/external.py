1ad68c83a0fee5ada7eb8b50455d83f05f9c223a
Ilya Tzoop <tolya12345w@gmail.com>
2022-06-01 01:04:21 +0500

    add external

---
+++ external/external.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+
+
+def encode_bzip(input_file, output_file, args=[]):
+    out_file = open(output_file, mode="wb")
+    process = subprocess.Popen(["bzip2", "-c", input_file] + args, stdout=out_file)
+    process.wait()
+    out_file.close()
+
+
+def encode_rar(input_file, output_file, args=[]):
+    process = subprocess.Popen(
+        ["rar", "a", output_file, input_file] + args, stdout=os.devnull
+    )
+    process.wait()
+
+
+def encode_7z(input_file, output_file, args=[]):
+    process = subprocess.Popen(
+        ["7z", "a", output_file, input_file] + args, stdout=os.devnull
+    )
+    process.wait()
+
+
+def encode_gzip(input_file, output_file):
+    out_file = open(output_file, mode="wb")
+    process = subprocess.Popen(["gzip", "-c", input_file], stdout=out_file)
+    process.wait()
+    out_file.close()
