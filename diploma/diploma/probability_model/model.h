1c62bd54680c6569860ef9907d31d1b05cecc8b7
Ilya Tzoop <tolya12345w@gmail.com>
2023-05-29 17:07:42 +0200

    refactor: refactor token frequency calculation and return type
    - Change `Preprocess` function to return a vector of `Token` objects instead of `int`
    - Change `GetFrequencies` function to use `GetProbabilities` and convert them to integer frequencies
    - Add `probabilities_to_frequencies` helper function to `Model` class
    - Remove TODO comment about adding default implementation for `GetFrequencies`
    
    Signed-off-by: Ilya Tzoop <tolya12345w@gmail.com>

--- diploma/diploma/probability_model/model.h
+++ diploma/diploma/probability_model/model.h
@@ -1,4 +1,7 @@
 #include <vector>
+#include <cmath>
+#include <numeric>
+#include <algorithm>
 #include <string>
 
 struct Token {
@@ -7,10 +10,32 @@ struct Token {
 
 class Model {
 public:
-    virtual std::vector<int> Preprocess(const std::string& text) = 0;
+    virtual std::vector<Token> Preprocess(const std::string& text) = 0;
     virtual void Feed(const std::vector<Token>& next_tokens) = 0;
-    // TODO: add default implementation
-    virtual std::vector<std::vector<double>> GetFrequencies() = 0;
+    virtual std::vector<std::vector<double>> GetProbabilities() = 0;
     virtual std::string Postprocess(const std::vector<Token>& tokens) = 0;
     virtual Token GetEndOfFileToken() = 0;
+
+    std::vector<std::vector<int>> GetFrequencies() {
+        auto probabilities = this->GetProbabilities();
+        std::vector<std::vector<int>> frequencies(probabilities.size());
+        for (auto& prob: probabilities) {
+            frequencies.push_back(probabilities_to_frequencies(prob, 32));
+        }
+        return frequencies;
+    }
+
+private:
+    static std::vector<int> probabilities_to_frequencies(std::vector<double> probabilities, int num_bits) {
+        std::vector<int> frequencies(probabilities.size(), 0);
+        double factor = std::pow(2, num_bits) - 3;
+        double cumulative = 0;
+        std::transform(probabilities.begin(), probabilities.end(), frequencies.begin(),
+                       [&cumulative, &factor](double prob) {
+                           cumulative += std::ceil(prob * factor) + 2;
+                           return static_cast<int>(cumulative);
+                       }
+        );
+        return frequencies;
+    }
 };
