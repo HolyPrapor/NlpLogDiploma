3c8bcfc68addc1b09b29311f0db45955d0abba05
Ilya Tzoop <tolya12345w@gmail.com>
2023-05-29 16:14:14 +0200

    add smart_coder and remove unused methods from coder.h

--- diploma/diploma/log_model/coder.h
+++ diploma/diploma/log_model/coder.h
@@ -20,9 +20,6 @@ public:
     virtual std::vector<size_t>
     EncodeLink(const size_t &record_index, const size_t &start_index, const size_t &length) = 0;
 
-    virtual std::tuple<size_t, size_t, size_t, size_t>
-    DecodeLink(const std::string &record, const size_t &index) = 0;
-
     /**
      *  У size было дефолтное значение None, но кажется, что оно бессмысленно.
      */
@@ -31,27 +28,13 @@ public:
     /**
      *  У size было дефолтное значение None, но кажется, что оно бессмысленно.
      */
-    virtual std::tuple<size_t, size_t> DecodeInt(const size_t &value, const size_t& size) = 0;
+    virtual std::tuple<size_t, size_t> DecodeInt(const std::vector<size_t> &record, const size_t &i, const size_t& size) = 0;
 
     virtual size_t
     DecodeIntFromStream(std::shared_ptr<BitInputStream> stream, const size_t &size) = 0;
 
     virtual std::tuple<size_t, size_t, size_t>
     DecodeLinkFromStream(std::shared_ptr<BitInputStream> stream) = 0;
-
-    virtual std::vector<size_t> EncodeToken(const unsigned char &symbol) = 0;
-
-    virtual std::tuple<size_t, size_t>
-    DecodeToken(const std::string &record, const size_t &index) = 0;
-
-    virtual bool ShouldDecodeAsLink(const std::string &record, const size_t &index) = 0;
-
-    /**
-     * В коде на Питоне здесь были еще параметры record и start, но никто эти параметры
-     * не использовал
-     */
-    virtual bool ShouldEncodeAsLink(const size_t &record_index, const size_t &start_index,
-                                    const size_t &length) = 0;
 
     [[nodiscard]] bool IsBinary() const {
         return is_binary_;
